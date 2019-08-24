06b9eee788eab27fcdf385aa31ab16f472fe50b8
mohamed sabry <mohamed.sabry@mail.com>
2019-08-24 18:51:04 +0200

    return user data after login

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -22,6 +22,7 @@ class UserController extends Controller
     {
         if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
             $user = Auth::user();
+            $success          = $user;
             $success['token'] = $user->createToken('MyApp')->accessToken;
             return response()->json(['success' => $success], $this->successStatus);
         } else {
